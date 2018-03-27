a5893db7657d40e12ebb5423ddb955671333d092
Micadurp <neoswed@hotmail.com>
2018-03-27 21:19:12 +0200

    adjusting slack file a bit

--- SHODAN.py
+++ SHODAN.py
@@ -5,32 +5,38 @@ usingDiscord = False
 
 try:
     import IRCHandler.ircHandler as ircHandler
+    print("Using IRC")
     usingIRC = True
 except ImportError:
     pass
-    
+
 try:
     import SlackHandler.slackHandler as slackHandler
+    print("Using Slack")
     usingSlack = True
 except ImportError:
     pass
 
 try:
     import DiscordHandler.discordHandler as discordHandler
+    print("Using Discord")
     usingDiscord = True
 except ImportError:
     pass
 
 if usingIRC:
     ircMsgHandler = ircHandler.MsgHandler()
-    #ircMsgHandler.connect()
+    # ircMsgHandler.connect()
 
 if usingSlack:
     slackHandler = slackHandler.MsgHandler()
     slackHandler.connect()
 
+
 if usingDiscord:
     discordHandler = discordHandler.MsgHandler()
     discordHandler.connect()
 
-    
+while True:
+    if usingSlack:
+        slackHandler.read_chat()
