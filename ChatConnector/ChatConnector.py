34fd74067a760c97faec6e192ba54157094e4a18
Micadurp <neoswed@hotmail.com>
2018-12-13 10:03:10 +0100

    Base irc connection
    Some copied code from a friend of mine, fix and adjust later

--- ChatConnector/ChatConnector.py
+++ ChatConnector/ChatConnector.py
@@ -4,9 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 
 usingIRC = False
 usingSlack = False
-usingDiscord = False
+
 try:
-    from IRCHandler import ircHandler
+    from .IRCHandler import ircHandler
     print("Using IRC")
     usingIRC = True
 except ImportError:
@@ -19,41 +19,30 @@ try:
 except ImportError:
     pass
 
-try:
-    from .DiscordHandler import discordHandler
-    print("Using Discord")
-    usingDiscord = True
-except ImportError:
-    pass
-
 class ChatHandler:
     def __init__(self):
-        #self.ircMsgHandler = ircHandler.MsgHandler(self.send_messages)
-        self.slackMsgHandler = slackHandler.MsgHandler(self.send_messages)
-        self.discordMsgHandler = discordHandler.MsgHandler(self.send_messages)
+        if usingIRC:
+            self.ircMsgHandler = ircHandler.MsgHandler(self.send_messages)
+        if usingSlack:
+            self.slackMsgHandler = slackHandler.MsgHandler(self.send_messages)
 
     def send_messages(self, message, source):
-        if source != "discord":
-            self.discordMsgHandler.send_message(message)
-        if source != "slack":
-            self.slackMsgHandler.send_message(message)
+        
+        if usingSlack:
+            if source != "slack":
+                self.slackMsgHandler.send_message(message)
+        if usingIRC:
+            if source != "irc":
+                self.ircMsgHandler.send_message(message)
 
     def start(self):       
         executor = ThreadPoolExecutor(2)
         loop = asyncio.get_event_loop()
 
-        #if usingIRC:
-            #ircMsgHandler.connect()
+        if usingIRC:
+            self.ircMsgHandler.connect()
 
         if usingSlack:
             self.slackMsgHandler.connect()
             asyncio.ensure_future(loop.run_in_executor(executor, self.slackMsgHandler.read_chat))
-
-        if usingDiscord:
-            asyncio.ensure_future(self.discordMsgHandler.connect)
-
-        try:
-            loop.run_forever()
-        except:
-            print("eeeyyy")
-            #discordHandler.logout()
+        
--- ChatConnector/IRCHandler/ircHandler.py
+++ ChatConnector/IRCHandler/ircHandler.py
@@ -3,8 +3,44 @@ import socket
 from ..Config import ircConfig
 import select
 
+
 class MsgHandler:
+    send_general_message = None
+    irc = None
+    
+    def __init__(self, general_send_message):
+        self.general_send_message = general_send_message
+
+    def checkforPing(self, p_checkforSec):
+        ready = select.select([self.irc], [self.irc], [self.irc], p_checkforSec)
+        if ready[0]:
+            text = self.irc.recv(2040)
+            text = text.decode('utf-8')
+            print (text)
+            
+            if text.find('PING') != -1:
+                print ("ping ponging")
+                self.irc.send(bytes('PONG ' + text.split()[1] + '\r\n', 'utf-8'))
+    
     def connect(self):
-        irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print ("Connecting to " + ircConfig.server)
-        irc.connect((ircConfig.server, 6667))
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print ("Connecting to " + ircConfig.SERVER)
+        self.irc.connect((ircConfig.SERVER, 6667))
+        self.checkforPing(2)
+        print ("sending user")
+        self.irc.send(bytes("USER "+ ircConfig.NICK +  " " + ircConfig.NICK + "2 " + ircConfig.NICK + "3 " + ircConfig.NICK + "3" + ircConfig.STOPSIGN, 'utf-8'))
+        print ("sending nick")
+        self.irc.send(bytes("NICK " + ircConfig.NICK + ircConfig.STOPSIGN, 'utf-8'))
+        self.checkforPing(4)
+        print ("joining channel")
+        self.irc.send(bytes("JOIN " + ircConfig.CHANNEL + ircConfig.STOPSIGN, 'utf-8'))
+        
+        admin =[]
+
+        allowCommands = True
+        self.checkforPing(4)
+
+        self.irc.send(bytes("PRIVMSG " + ircConfig.CHANNEL + " :" + "HI BYE" + ircConfig.STOPSIGN, 'utf-8'))
+
+    def send_message(self, message):
+        print("irc send message not implemented")
